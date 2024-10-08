f0d0cb3fab0d189ca916f66daac5b9d9009ddaac
JafrulSadik <jafrul.sadik4@gmail.com>
2024-10-09 05:10:23 +0600

    Move public and vercel folder to root directory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import http from "node:http";
-import app from "./app";
+import express, { Request, Response } from "express";
 import config from "./config/config";
 import dbConnect from "./config/db";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import router from "./routes";
 
-const server = http.createServer(app);
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Everything is ok.",
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Everything is ok.",
+  });
+});
+
+// Use the combined router
+app.use(router);
+
+app.use(globalErrorHandler);
 const port = config.port || 5110;
 
 const main = async () => {
@@ -12,7 +32,7 @@ const main = async () => {
     await dbConnect();
     console.log("DB connection successfull !!!");
 
-    server.listen(port, () => {
+    app.listen(port, () => {
       console.log(`Server is listening on port ${port}.`);
     });
   } catch (error) {
@@ -23,4 +43,4 @@ const main = async () => {
 
 main();
 
-export default server;
+export default app;
