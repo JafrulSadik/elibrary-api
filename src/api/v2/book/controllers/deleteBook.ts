36c6759b9f69be35980ed7fc0f5d52b6bd828801
JafrulSadik <jafrul.sadik4@gmail.com>
2025-02-20 01:21:18 +0600

    refactor: remove cloudinary file handling from deleteBook and updateBook controllers

--- src/api/v2/book/controllers/deleteBook.ts
+++ src/api/v2/book/controllers/deleteBook.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import cloudinary from "../../../../config/cloudinary";
 import Book from "../../../../models/Book";
 import { notFound, serverError } from "../../../../utils";
 
@@ -14,42 +13,6 @@ export const deleteBook = async (
 
     if (!book) {
       return next(notFound("Book not found!"));
-    }
-
-    // Remove cover image file form coudinary
-    const bookCoverImgUrl = book.cover;
-    const coverImgSplitUrl = bookCoverImgUrl.split("/");
-    //   Generate public id form url
-    const coverImgPublicId =
-      coverImgSplitUrl.at(-3) +
-      "/" +
-      coverImgSplitUrl.at(-2) +
-      "/" +
-      coverImgSplitUrl.at(-1)?.split(".").at(-2);
-
-    try {
-      await cloudinary.api.delete_resources([coverImgPublicId]);
-    } catch (error) {
-      return next(serverError("Removing file form cloudinary faield."));
-    }
-
-    // Remove pdf file from cloudinary
-    const bookPdfFileUrl = book.file;
-    const pdfSplitUrl = bookPdfFileUrl.split("/");
-    //   Generate public id form url
-    const bookPublicId =
-      pdfSplitUrl.at(-3) + "/" + pdfSplitUrl.at(-2) + "/" + pdfSplitUrl.at(-1);
-
-    try {
-      await cloudinary.api.delete_resources([bookPublicId], {
-        resource_type: "raw",
-      });
-    } catch (error) {
-      return next(
-        serverError(
-          "An error occurred while trying to delete the previous file."
-        )
-      );
     }
 
     await Book.findByIdAndDelete(bookId);
